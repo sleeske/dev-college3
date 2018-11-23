b34dae11eabc6f04688cd54d94c8f7c85301203d
Jan Sliski <j.sliski@merixstudio.com>
2018-11-23 23:08:14 +0100

    added working email backend

--- testproject/testapp/backends/basic_email.py
+++ testproject/testapp/backends/basic_email.py
@@ -1,5 +1,21 @@
+from django.core.mail import send_mail
+
 from trench.backends import AbstractMessageDispatcher
+from trench.settings import api_settings
 
 
 class BasicEmailBackend(AbstractMessageDispatcher):
-    pass
+    def dispatch_message(self, *args, **kwargs):
+        """Sends an email with verification code"""
+
+        code = self.create_code()
+
+        send_mail(
+            'Your verification code',
+            f'Your verification code is {code}',
+            api_settings.FROM_EMAIL,
+            [self.to],
+            fail_silently=False,
+        )
+
+        return {'message', 'Email message with MFA code has been sent'}
